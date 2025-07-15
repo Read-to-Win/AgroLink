1ed5fdb25bd555e1cf80990444c1df60628a8941
PreciousXena <precioushenryc@gmail.com>
2025-07-15 14:40:28 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./App.css";
 import Landing from "./pages/Landing";
-import FarmerHome from "./pages/farmer/FarmerHome";
-import DashboardLayout from "./layouts/DashboardLayout";
-import FarmerAds from "./pages/farmer/FarmerAds";
-import CreateAd from "./pages/farmer/CreateAd";
 import Join from "./pages/Join";
-import SignIn from "./pages/SignIn";
-import FarmerForm from "./pages/FarmerForm";
-import EditForm from "./pages/EditForm";
-import Notifications from "./pages/farmer/Notification";
 import ProductDetails from "./pages/ProductDetails";
 import ProductsPage from "./pages/ProductsPage";
-import ViewDetail from "./pages/ViewDetails";
 import Farmer from "./pages/Farmer";
-import AdminSignUp from "./pages/AdminSignUp";
-import DashboardLayout from "./layouts/DashboardLayout";
 import Dashboard from "./pages/Dashboard";
+import DashboardLayout from "./layouts/DashboardLayout";
 import Products from "./pages/Products";
 import CreateAd from "./pages/CreateAd";
 import Notifications from "./pages/Notifications";
@@ -29,9 +19,8 @@ function App() {
       element: <Landing />,
     },
     {
-      path:"/farmer",
-      element: <Farmer/>
-      element: <AdminSignUp />,
+      path: "/farmer",
+      element: <Farmer />,
     },
     {
       path: "/dashboard",
@@ -39,15 +28,8 @@ function App() {
       children: [
         {
           index: true,
-          element: <FarmerHome />,
+          element: <Dashboard />,
         },
-        {
-          path: "adverts",
-          element: <FarmerAds />,
-        },
-        {
-          path: "create-add",
-        { index: true, element: <Dashboard /> },
 
         {
           path: "all-adverts",
@@ -63,8 +45,8 @@ function App() {
           element: <Notifications />,
         },
         {
-          path: "edit/:id",
-          element: <EditForm />,
+          path: "messages",
+          element: <MyMessages />,
         },
       ],
     },
@@ -72,36 +54,12 @@ function App() {
       path: "/join",
       element: <Join />,
     },
-    {
-      path: "/sign-in",
-      element: <SignIn />,
-    },
-    {
-      path: "/farmerform",
-      element: <FarmerForm />,
-    },
-
-    {
-      path: "/edit",
-      element: <EditForm />,
-    },
 
     { path: "/product/:id", element: <ProductDetails /> },
 
     {
       path: "/products",
       element: <ProductsPage />,
-    },
-    {
-      path: "/ad-detail/:id",
-      element: <ViewDetail />,
-    },
-  ]);
-
-          path: "messages",
-          element: <MyMessages />,
-        },
-      ],
     },
   ]);
   return <RouterProvider router={router} />;
